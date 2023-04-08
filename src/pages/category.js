00ce70067df545edd8612b367aad34c9b5d249e0
SiyanPu <pusiyan0117@163.com>
2023-04-08 17:43:44 -0400

    set up routes and category page

---
+++ src/pages/category.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Category() {
+  const categories = [
+    { id: 1, name: 'Category 1' },
+    { id: 2, name: 'Category 2' },
+    { id: 3, name: 'Category 3' },
+    { id: 4, name: 'Category 4' },
+  ];
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        {categories.map(category => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Category;
+
