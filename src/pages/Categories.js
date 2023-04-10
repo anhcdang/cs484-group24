73ea1b4f564d53118551d2717abac2a055488830
SiyanPu <pusiyan0117@163.com>
2023-04-09 23:27:52 -0400

    create category page draft

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/styles.css';
+import sample_img from '../static/images/sample.png';
 
 function Categories() {
-  const categories = [
-    { id: 1, name: 'Category 1' },
-    { id: 2, name: 'Category 2' },
-    { id: 3, name: 'Category 3' },
-    { id: 4, name: 'Category 4' },
+
+  const cards = [
+    { category_type_id: 1, title: 'Social', src: sample_img },
+    { category_type_id: 2, title: 'Academics', src: sample_img },
+    { category_type_id: 3, title: 'Sports', src: sample_img },
+    { category_type_id: 4, title: 'Music', src: sample_img },
+    { category_type_id: 5, title: 'Cultural', src: sample_img},
+    { category_type_id: 6, title: 'Arts', src: sample_img },
   ];
 
+  const handleCardClick = (request_type_id) => {
+    
+  };
+
   return (
-    <div>
-      <h1>Categories</h1>
-      <ul>
-        {categories.map(category => (
-          <li key={category.id}>{category.name}</li>
+    <div className="container-fluid">
+      <div className="row row-cols-3">
+        {cards.map((card) => (
+          <div key={card.category_type_id} className="col">
+            <Card onClick={() => handleCardClick(card.category_type_id)}>
+              <Card.Img variant="top" src={card.src} />
+              <Card.Body>
+                <Card.Title>{card.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
+
+
 
 export default Categories;
