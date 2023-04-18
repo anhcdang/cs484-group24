de4674c492b62df9924abb2e0e14939cebf9767d
Anh Dang <anhcdang@gmail.com>
2023-04-17 20:59:32 -0400

    initial kinect processing implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useEffect } from "react";
 import './static/styles.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Categories from './pages/Categories';
 import Carousel from './pages/Carousel';
+import { frames } from './sketch';
 // import SampleCardComponent from './components/SampleCardComponent';
 
 import Stack from 'react-bootstrap/Stack';
@@ -11,6 +13,11 @@ function App() {
   // const sampleCardText1 = "To go to the Categories page, add \"/categories\" to the end of the url."
   // const sampleCardText2 = "To go to the Carousel page, add \"/carousel\" to the end of the url."
 
+  useEffect(() => {
+    frames.start()
+    console.log("component mounted")
+  }, []);
+  
   return (
     <Stack style={{height: '100vh', 'width': '100vw'}}>
       <Router>
---
+++ src/sketch.js
@@ -0,0 +1,102 @@
+const host = "cpsc484-03.yale.internal:8888";
+
+const DOWN = "DOWN"
+const UPPER_LEFT = "UPPER LEFT"
+const LOWER_LEFT = "LOWER LEFT"
+const UPPER_RIGHT = "UPPER RIGHT"
+const LOWER_RIGHT = "LOWER RIGHT"
+
+export const frames = {
+    socket: null,
+  
+    start: function() {
+      var url = "ws://" + host + "/frames";
+      frames.socket = new WebSocket(url);
+      frames.socket.onmessage = function (event) {
+        var command = frames.get_wrist_command(JSON.parse(event.data));
+        if (command !== null) {
+          sendWristCommand(command);
+        }
+      }
+    },
+  
+    get_right_wrist_command: function (frame) {
+      var command = null;
+      if (frame.people.length < 1) {
+        return command;
+      }
+  
+      // Normalize by subtracting the root (pelvis) joint coordinates
+      var pelvis_x = frame.people[0].joints[0].position.x;
+      var pelvis_y = frame.people[0].joints[0].position.y;
+      var pelvis_z = frame.people[0].joints[0].position.z;
+      var right_wrist_x = (frame.people[0].joints[14].position.x - pelvis_x) * -1;
+      var right_wrist_y = (frame.people[0].joints[14].position.y - pelvis_y) * -1;
+      var right_wrist_z = (frame.people[0].joints[14].position.z - pelvis_z) * -1;
+      var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
+      var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
+      var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
+  
+      if (right_wrist_z < 100 || left_wrist_z < 100) {
+        return command;
+      }
+
+      // CURRENT DISPLAY LOGIC??:
+      //            -200          200
+      // -----------------------------------------
+      // |            |            |             |
+      // | upper left |            | upper right |
+      // |            |            |             |
+      // -----------------------------------------  500
+      // |            |            |             |
+      // |            |            |             |
+      // |            |            |             |
+      // -----------------------------------------  100
+      // |            |            |             |
+      // | lower left |    down    | lower right |
+      // |            |            |             |
+      // -----------------------------------------
+
+      // NEED TO TEST, NUMBERS ARE NOT FINAL
+      if ((right_wrist_x < 200 && right_wrist_x > -200) || (left_wrist_x < 200 && left_wrist_x > -200)) {
+        if (right_wrist_y < 100 || left_wrist_y < 100) {
+          command = DOWN;
+        }
+      } else if (left_wrist_x < -200) {
+        if (left_wrist_y > 500) {
+            command = UPPER_LEFT
+        } else if (left_wrist_y < 100) {
+            command = LOWER_LEFT
+        }
+      } else if (right_wrist_x > 200) {
+        if (right_wrist_y > 500) {
+            command = UPPER_RIGHT
+        } else if (right_wrist_y < 100) {
+            command = LOWER_RIGHT
+        }
+      }
+      return command;
+    }
+  };
+
+  function sendWristCommand(command) {
+    switch (command) {
+      case DOWN:
+        console.log(DOWN);
+        break;
+      case UPPER_LEFT:
+        console.log(UPPER_LEFT);
+        break;
+      case LOWER_LEFT:
+        console.log(LOWER_LEFT);
+        break;
+      case UPPER_RIGHT:
+        console.log(UPPER_RIGHT)
+        break;
+      case LOWER_RIGHT:
+        console.log(LOWER_RIGHT);
+        break;
+      default:
+        break;
+    }
+  }
