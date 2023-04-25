2e83cc46478e0a941c660bfcb89319246de036fc
sianna <sianna@Siannas-MacBook-Air.local>
2023-04-24 20:34:24 -0400

    start page

---
+++ src/pages/Start.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../static/styles.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="circle-container">
+        <div className="circle">Start</div>
+      </div>
+      <div className="text-large">Hello</div>
+      <div className="text-small">Wave your hand over the 'Start' to view campus events</div>
+    </div>
+  );
+}
+
+export default App;
